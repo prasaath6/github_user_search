4c7bd3c581367561cb08172b3d72718a47f595b3
Prasaath <prasaath@localhost.localdomain>
2018-05-23 01:04:15 +0530

    task completed

--- script.js
+++ script.js
@@ -6,22 +6,59 @@ app.constant('config', {
 });
 app.controller('github_app', function ($scope, $rootScope, config, gitService) {
     $scope.userData = [];
-    $scope.getlist = function (query) {
-        gitService.getlist(query)
-            .then(function (response) {
-                $scope.userData = response;
-                console.log(response)
-            }).catch(function (response) {
-                console.log(response.status);
-            });
-    };
+    $scope.query = "";
+    $scope.page = 1;
+    $scope.nextcall = 1;
+    $scope.helptext = "Search for the github users!!!"
+    $scope.getlist = function () {
+        if (!$scope.query) {
+            alert("Query Should Not Empty!!");
+        } else {
+            let lastquery = localStorage.getItem('last_query')
+            if (lastquery != $scope.query) {
+                $scope.userData = [];
+                $scope.page = 1;
+                localStorage.setItem("last_query", $scope.query)
+            } else {
+                console.log("kjh");
+            }
+            gitService.getlist($scope.query, $scope.page)
+                .then(function (response) {
+                    let listdata = response.items;
+                    if (listdata != "") {
+                        angular.forEach(listdata, function (value, key) {
+                            $scope.userData.push(value);
+                        });
+                        $scope.nextcall = 1;
+                        $scope.page++;
+                    } else {
+                        $scope.nextcall = 0;
+                    }
+
+                }).catch(function (response) {
+                    console.log(response.status);
+                });
+
+        };
+    }
+    $(window).scroll(function () {
+        var bodypos = $("body")[0].scrollHeight;
+        var windowh = $(window).height();
+        bodypos = bodypos - windowh;
+        var windowpos = $(window).scrollTop();
+        var persentage = Math.round(((windowpos / bodypos) * 100));
+        if (Math.round(persentage) > 80 && $scope.nextcall == 1) {
+            $scope.nextcall = 0;
+            $scope.getlist();
+        }
+    });
 });
 app.factory('gitService', function ($http, $q, config) {
     return {
-        getlist: function (query) {
+        getlist: function (query, page) {
             var def = $q.defer();
 
-            $http.get(config.search + "?q=pra")
+            $http.get(config.search + "?q=" + query + "&page=" + page)
                 .then(function (response) {
                     def.resolve(response.data);
                 })
